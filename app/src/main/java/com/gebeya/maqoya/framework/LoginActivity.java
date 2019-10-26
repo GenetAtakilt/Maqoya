9bc02404c94df7d6ab264ac6f4fcbe93bc7e132c
genet <genetatakilt.g@gmail.com>
2019-10-26 09:13:18 -0700

    homepage added

--- app/src/main/java/com/gebeya/maqoya/framework/LoginActivity.java
+++ app/src/main/java/com/gebeya/maqoya/framework/LoginActivity.java
@@ -3,10 +3,9 @@ package com.gebeya.maqoya.framework;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.annotation.Nullable;
-
 import com.gebeya.maqoya.framework.base.BaseActivity;
 
 public class LoginActivity extends BaseActivity {
@@ -15,6 +14,7 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         TextView register;
+        Button  login;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
@@ -25,6 +25,15 @@ public class LoginActivity extends BaseActivity {
             public void onClick(View v) {
 
                 Intent  intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity1.class);
                 startActivity(intent);
             }
         });
