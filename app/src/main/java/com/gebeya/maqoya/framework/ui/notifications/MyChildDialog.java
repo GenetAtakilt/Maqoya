e431fa83229d3f625006c48eed600f68ef040566
genet <genetatakilt.g@gmail.com>
2019-10-30 15:45:56 -0700

    updated

---
+++ app/src/main/java/com/gebeya/maqoya/framework/ui/notifications/MyChildDialog.java
@@ -0,0 +1,24 @@
+package com.gebeya.maqoya.framework.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.gebeya.maqoya.framework.R;
+
+public class MyChildDialog extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.dialog_child_info,container,false);
+
+        return view;
+    }
+}
