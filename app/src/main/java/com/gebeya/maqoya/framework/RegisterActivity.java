a6902fc31978f51036d3a66bed9c444dfc9569ba
genet <genetatakilt.g@gmail.com>
2019-11-03 15:11:14 -0800

    API connection

--- app/src/main/java/com/gebeya/maqoya/framework/RegisterActivity.java
+++ app/src/main/java/com/gebeya/maqoya/framework/RegisterActivity.java
@@ -2,15 +2,20 @@ package com.gebeya.maqoya.framework;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.gebeya.maqoya.framework.base.BaseActivity;
 
-public class RegisterActivity extends BaseActivity {
+public class RegisterActivity extends BaseActivity implements View.OnClickListener {
 
+  EditText editTextEmail, editTextPassword, editTextConfirmPassword;
+  Button register;
   TextView login;
 
   @Override
@@ -18,15 +23,65 @@ public class RegisterActivity extends BaseActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_register);
 
+    editTextEmail = findViewById(R.id.email);
+    editTextPassword = findViewById(R.id.password);
+    editTextConfirmPassword = findViewById(R.id.confirm_password);
+    register = findViewById(R.id.register);
     login = findViewById(R.id.loginTo);
-    login.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
 
-        Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
-        startActivity(intent);
-      }
-    });
+
+    register.setOnClickListener(this);
+    login.setOnClickListener(this);
+
+//    login.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View v) {
+//
+//        Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+//        startActivity(intent);
+//      }
+//    });
+
+  }
+
+  private void parentRegistration()
+  {
+    String email = editTextEmail.getText().toString().trim();
+    String password = editTextPassword.getText().toString().trim();
+    String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+
+    if(email.isEmpty())
+    {
+      editTextEmail.setError("Email is required");
+      editTextEmail.requestFocus();
+      return;
+    }
+
+    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+    {
+      editTextEmail.setError("Enter a valid Email");
+      editTextEmail.requestFocus();
+      return;
+    }
+
+  }
+
+
+
+  @Override
+  public void onClick(View v) {
+
+    switch (v.getId())
+    {
+      case R.id.register:
+
+        parentRegistration();
+        break;
+
+      case  R.id.loginTo:
+        break;
+    }
 
   }
 }
--- app/src/main/java/com/gebeya/maqoya/framework/WelcomeActivity.java
+++ app/src/main/java/com/gebeya/maqoya/framework/WelcomeActivity.java
@@ -3,15 +3,27 @@ package com.gebeya.maqoya.framework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.gebeya.maqoya.framework.base.BaseActivity;
 
+import butterknife.BindView;
+
 public class WelcomeActivity extends BaseActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
     }
 
 
